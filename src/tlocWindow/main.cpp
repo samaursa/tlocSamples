3d6204cd9993ab05da6bab75a9ea591e8021e0e9
samaursa <samaursa@localhost>
2013-01-24 20:26:58 -0500

    [ADD][OBJ] tlocWindow sample project completed (TESTED) - tlocWindow         * Simple sample showing how to create a window with callbacks.

---
+++ src/tlocWindow/main.cpp
@@ -0,0 +1,51 @@
+#include <tlocCore/tloc_core.h>
+#include <tlocGraphics/tloc_graphics.h>
+
+#include <tlocCore/memory/tlocLinkMe.cpp>
+
+using namespace tloc;
+
+class WindowCallback
+{
+public:
+  WindowCallback()
+    : m_endProgram(false)
+  { }
+
+  void OnWindowEvent(const gfx_win::WindowEvent& a_event)
+  {
+    if (a_event.m_type == gfx_win::WindowEvent::close)
+    { m_endProgram = true; }
+
+    if (a_event.m_type == gfx_win::WindowEvent::gained_focus)
+    { printf("\nWindow just gained focus"); }
+
+    if (a_event.m_type == gfx_win::WindowEvent::lost_focus)
+    { printf("\nWindow just lost focus"); }
+
+    if (a_event.m_type == gfx_win::WindowEvent::resized)
+    { printf("\nWindow just resized"); }
+  }
+
+  bool  m_endProgram;
+};
+TLOC_DEF_TYPE(WindowCallback);
+
+int main()
+{
+  gfx_win::Window win;
+  WindowCallback  winCallback;
+  win.Register(&winCallback);
+
+  win.Create( gfx_win::Window::graphics_mode::Properties(800, 600),
+              gfx_win::WindowSettings("tlocWindow") );
+
+  while (win.IsValid() && !winCallback.m_endProgram)
+  {
+    gfx_win::WindowEvent  evt;
+    while (win.GetEvent(evt))
+    { }
+
+    win.SwapBuffers();
+  }
+}
