5e147de0525d47f2d30e52d514dd7fd4b346a6ff
samaursa <saadrustam@gmail.com>
2013-04-04 22:34:45 -0400

    [INCR][FIX][OBJ] tlocInput sample was clashing with tlocInput library (FIXED) | tlocTexturedFan works with a fresh CMake build on the simulator (TESTED) - Other samples do not work yet due to the way input manager is instantiated. - tlocTextureFan 	* The CMakeLists of the sample now has add_apple_plist which allows the             project to fill out the necessary plist variables. Note that that the             add_apple_plist function was NOT working because the variables set in             the function were not declared to be global (i.e. to be passed to parent scope).             This caused the infamous "failed to attach to process ID" error.

--- tlocUserVars.cmake
+++ tlocUserVars.cmake
@@ -1,3 +1,3 @@
-set(USER_EXECUTABLE_PROJECTS "tlocInput;tlocInputCallbacks;tlocSimplePhysics;tlocSimpleQuad;tlocTexturedFan;tlocWindow")
+set(USER_EXECUTABLE_PROJECTS "tlocSimpleInput;tlocInputCallbacks;tlocSimplePhysics;tlocSimpleQuad;tlocTexturedFan;tlocWindow")
 set(USER_LIBRARY_PROJECTS "")
 set(USER_ASSETS_PATH "${CMAKE_SOURCE_DIR}/src/samplesAssetsPath.h")
