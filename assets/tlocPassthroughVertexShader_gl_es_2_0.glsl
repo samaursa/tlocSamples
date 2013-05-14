5120151b92762d56ff2176e1d838e87400744927
samaursa <saadrustam@gmail.com>
2013-05-14 00:26:07 -0400

    [FIX][MIN] Fixed GLES vertex shader (TESTED on iOS)

--- assets/tlocPassthroughVertexShader_gl_es_2_0.glsl
+++ assets/tlocPassthroughVertexShader_gl_es_2_0.glsl
@@ -1,7 +1,7 @@
 #version 100
 
 // Input vertex data, different for all executions of this shader.
-attribute vec3 a_vPos;
+attribute lowp vec3 a_vPos;
 uniform mat4 u_mvp;
 
 varying lowp vec3 v_color;
@@ -9,5 +9,5 @@ varying lowp vec3 v_color;
 void main()
 { 
   gl_Position = u_mvp * vec4(a_vPos, 1);
-  v_color = vec3(a_vPos[0], a_vPos[1], 0.3f);
+  v_color = vec3(a_vPos[0], a_vPos[1], 0.3);
 }
