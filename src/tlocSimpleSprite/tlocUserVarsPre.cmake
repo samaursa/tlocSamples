802028ff4c09cef94ec1f349f4bb37543f6b2594
samaursa <saadrustam@gmail.com>
2015-01-01 23:38:05 -0500

    [MAJ][MOD] CMakeLists.txt modified and updated to improved versions in the engine (TESTED)

--- src/tlocSimpleSprite/tlocUserVarsPre.cmake
+++ src/tlocSimpleSprite/tlocUserVarsPre.cmake
@@ -4,19 +4,19 @@
 # project.
 
 # Do NOT remove the following variables. Modify the variables to suit your project
-set(USER_SOURCE_FILES
+set(SOLUTION_SOURCE_FILES
   main.cpp
   )
 
 # Do not include individual assets here. Only add paths
-set(USER_ASSETS_PATH
+set(SOLUTION_ASSETS_PATH
   ../../assets
   )
 
 # Dependent project is compiled after dependency
-set(USER_PROJECT_DEPENDENCIES
+set(SOLUTION_PROJECT_DEPENDENCIES
   )
 
 # Libraries that the executable needs to link against
-set(USER_EXECUTABLE_LINK_LIBRARIES
+set(SOLUTION_EXECUTABLE_LINK_LIBRARIES
   )
--- src/tlocVersionCheck.cpp
+++ src/tlocVersionCheck.cpp
@@ -1,2 +1,2 @@
-#define TLOC_VERSION_0_1_13544_1420150801 
+#define TLOC_VERSION_0_1_13555_1420164747 
 #include "../tlocVersion.h"
